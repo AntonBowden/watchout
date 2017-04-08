6609f15de59f85c3805ca28f14ef0b9a4fb64496
Anton Bowden <bowden.anton@gmail.com>
2017-04-08 04:50:02 +0100

    Make a draggable player

--- client/watchout.js
+++ client/watchout.js
@@ -18,6 +18,7 @@ var gameBoard = d3.select('body')
                   .attr({width: options.width, height: options.height})
                   .style('background-color', options.background);
 
+
 //Enemies
 var enemiesStorage = [];
 for (var i = 0; i < 5; i++) {
@@ -29,7 +30,7 @@ var randomCoord = function() {
 };
 
 
-var enemies = gameBoard.selectAll('circle')
+var enemies = gameBoard.selectAll('enemy')
               .data(enemiesStorage)
               .enter()
                 .append('circle')
@@ -52,7 +53,44 @@ move();
 //End of Enemies
 
 //Player
+// var drag = d3.behavior.drag()
+//         .on('drag', function(d) {
+//           d.x += d3.event.dx;
+//           d.y += d3.event.dy;
+//           d3.select(this).attr('transform', function(d) {
+//             return 'translate(' + [ d.x, d.y ] + ')';
+//           });
+//         });
 
+
+
+
+var player = gameBoard.selectAll('player')
+            .data([1]) //, {'x': Math.random(), 'y': Math.random()}
+            .enter()
+            .append('circle')
+            .attr({cx: options.width / 2, cy: options.height / 2, r: 10, fill: 'orange'})
+            // .attr('transform', 'translate(' + 222 + ',' + 222 + ')')
+            .classed('player', true);
+
+
+
+var dragOn = function() {
+    // set position based on mouse position
+  position = [d3.event.x, d3.event.y];
+  redraw();
+};
+
+var redraw = function() {
+    // set circle's position based on internal variable
+  d3.select('.player')
+    .attr('cx', position[0])
+    .attr('cy', position[1]);
+};
+
+d3.behavior.drag()  // capture mouse drag event
+    .on('drag', dragOn)
+    .call(d3.select('.player'));
 
 
 
